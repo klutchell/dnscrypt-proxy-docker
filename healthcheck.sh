706fdc01bc78cb0c3bfd3198ee40e0e0cd6c8c23
Kyle Harding <kharding@marchnetworks.com>
2019-09-10 08:58:21 -0400

    increase healthcheck wait

--- healthcheck.sh
+++ healthcheck.sh
@@ -2,7 +2,7 @@
 
 nohup sh -c 'dnscrypt-proxy -config /config/dnscrypt-proxy.toml' &
 
-sleep 5
+sleep 10
 
 drill sigok.verteiltesysteme.net @127.0.0.1 | grep NOERROR
 drill sigfail.verteiltesysteme.net @127.0.0.1 | grep SERVFAIL
